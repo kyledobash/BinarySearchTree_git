6aef555af54974d6944e4255b587b8585797f0f9
kyledobash <kyledobash@yahoo.com>
2021-01-15 11:04:18 -0700

    Created the Node and BinarySearchTree classes, with skeletons.

---
+++ BinarySearchTree_actual/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree_actual
+{
+    class Node
+    {
+        // member variables
+        int data;
+        string position;
+
+        // constructor
+        public Node(int data, string position)
+        {
+            this.data = data;
+            this.position = position;
+        }
+
+        // member methods
+
+    }
+}
